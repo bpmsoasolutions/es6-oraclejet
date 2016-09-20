ec048da4f3658c1d4ef3f6aaa2c1518d03aba5ea
Daniel Biedma Ramos <info@dbr.io>
2016-09-20 08:41:10 +0200

    try to separate better the css tasks

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,89 +1,72 @@
-var gulp = require('gulp')
-var replace = require('gulp-replace')
-var es = require('event-stream')
-var concat = require('gulp-concat')
+import gulp from 'gulp'
+import replace from 'gulp-replace'
+import es from 'event-stream'
+import concat from 'gulp-concat'
 
 import configuration from '../configuration'
 
+gulp.task('assetsJET', ['images:jet-merges', 'css:jet-merges', 'fonts:jet-merges', 'assets:jet-common'], function () {})
 
-
-gulp.task('assetsJET', ['images:cordova', 'css:cordova', 'fonts:cordova'], function () {})
-gulp.task('images:cordova', function () {
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-android/images/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta-android'
-    }).pipe(gulp.dest('app/merges/android'));
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-ios/images/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta-ios'
-    }).pipe(gulp.dest('app/merges/ios'));
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-windows/images/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta-windows'
-    }).pipe(gulp.dest('app/merges/windows'));
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta/images/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta'
-    }).pipe(gulp.dest('app/merges/browser'));
-})
-gulp.task('css:cordova', function () {
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-android/oj-alta.css')
-        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
-        .pipe(gulp.dest('app/merges/android'));
-
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-ios/oj-alta.css')
-        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
-        .pipe(gulp.dest('app/merges/ios'));
-
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-windows/oj-alta.css')
-        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
-        .pipe(gulp.dest('app/merges/windows'));
-
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta/oj-alta.css')
-        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
-        .pipe(gulp.dest('app/merges/browser'));
+const jetCssPath = './src/bower_modules/oraclejet/dist/css'
+const platforms = [
+    { in: 'alta-android', out: 'android'},
+    { in: 'alta-ios', out: 'ios'},
+    { in: 'alta-windows', out: 'windows'},
+    { in: 'alta', out: 'browser'}
+]
+gulp.task('images:jet-merges', function () {
+    platforms.map((platform)=>{
+        gulp.src(`${jetCssPath}/${platform.in}/images/**/*`, {
+            base: `${jetCssPath}/${platform.in}`
+        }).pipe(gulp.dest(`${configuration.paths.cordovaMerges}${platform.out}`));
+    })
 })
 
-gulp.task('fonts:cordova', function () {
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-android/fonts/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta-android'
-    }).pipe(gulp.dest('app/merges/android'));
-
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-ios/fonts/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta-ios'
-    }).pipe(gulp.dest('app/merges/ios'));
-
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta-windows/fonts/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta-windows'
-    }).pipe(gulp.dest('app/merges/windows'));
-
-    gulp.src('./src/bower_modules/oraclejet/dist/css/alta/fonts/**/*', {
-        base: './src/bower_modules/oraclejet/dist/css/alta'
-    }).pipe(gulp.dest('app/merges/browser'));
+gulp.task('css:jet-merges', function () {
+    platforms.map((platform)=>{
+        gulp.src(`${jetCssPath}/${platform.in}/oj-alta.css`, {
+            base: `${jetCssPath}/${platform.in}`
+        }).pipe(gulp.dest(`${configuration.paths.cordovaMerges}${platform.out}`));
+    })
 })
 
+gulp.task('fonts:jet-merges', function () {
+    platforms.map((platform)=>{
+        gulp.src(`${jetCssPath}/${platform.in}/fonts/**/*`, {
+            base: `${jetCssPath}/${platform.in}`
+        }).pipe(gulp.dest(`${configuration.paths.cordovaMerges}${platform.out}`));
+    })
+})
 
-///_______
+gulp.task('assets:jet-common', function () {
+    let ojImgFilesCommon = gulp.src('./src/bower_modules/oraclejet/dist/css/common/**/*', {
+        base: './src/bower_modules/oraclejet/dist/css/'
+    })
+    let ojImgFilesSpecific = gulp.src('./src/bower_modules/oraclejet/dist/js/libs/oj/resources/**/*', {
+        base: './src/bower_modules/oraclejet/dist/js/libs/oj'
+    })
 
-
-gulp.task('css', function () {
-    var appCss = gulp.src('src/css/*.css'),
-        ojImgFilesCommon = gulp.src('./src/bower_modules/oraclejet/dist/css/common/**/*', {
-            base: './src/bower_modules/oraclejet/dist/css/'
-        }),
-        ojImgFilesSpecific = gulp.src('./src/bower_modules/oraclejet/dist/js/libs/oj/resources/**/*', {
-            base: './src/bower_modules/oraclejet/dist/js/libs/oj'
-        });
-    //Copy OJ NLS
     gulp.src('./src/bower_modules/oraclejet/dist/js/libs/oj/resources/nls/**/*', {
         base: './src/bower_modules/oraclejet/dist/js/libs/oj/resources'
     }).pipe(gulp.dest(configuration.paths.dist + 'ojtranslations/'))
 
+    return es.concat(ojImgFilesCommon, ojImgFilesSpecific)
+        .pipe(gulp.dest(configuration.paths.dist));
+})
+
+///_______
+gulp.task('css', function () {
+    let appCss = gulp.src('src/css/*.css')
+
     //Font awesome
-    var faCss = gulp.src('src/bower_modules/font-awesome/css/font-awesome.min.css')
-            .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/')),
-        faFonts = gulp.src('./src/bower_modules/font-awesome/fonts/**/*', {
-            base: './src/bower_modules/font-awesome/'
-        })
+    let faCss = gulp.src('src/bower_modules/font-awesome/css/font-awesome.min.css')
+        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
+    let faFonts = gulp.src('./src/bower_modules/font-awesome/fonts/**/*', {
+        base: './src/bower_modules/font-awesome/'
+    })
 
-    var combinedCss = es.concat(appCss, faCss).pipe(concat('css.css'))
+    let combinedCss = es.concat(appCss, faCss).pipe(concat('css.css'))
 
-    return es.concat(combinedCss, faFonts, ojImgFilesCommon, ojImgFilesSpecific)
+    return es.concat(combinedCss, faFonts)
         .pipe(gulp.dest(configuration.paths.dist));
 });
